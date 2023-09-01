3e1c74e694c7fe36ec65f67da11729f24337878b
Sirajul Islam <ayasabegum134@gmail.com>
2023-09-01 11:36:47 +0600

    my main part done

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-const getBtnDataCollect = async () =>{
+const getBtnDataCollect = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
     const btnData = data.data
     const allBtn = document.getElementById('receive-btn-data-api');
-    btnData.forEach((single) =>{
+    btnData.forEach((single) => {
         const div = document.createElement('div');
         div.innerHTML = `
-        <button onclick= "btnInfoData('${single.category_id}')" class="btn bg-[#FF1F3D] text-[#252525] hover:text-white hover:bg-gray-400">
+        <button onclick= "btnInfoData('${single.category_id}')" class="btn hover:bg-[#FF1F3D] text-[#252525] hover:text-white bg-gray-400">
         ${single.category}
         </button>
         `;
+
+
         allBtn.appendChild(div)
     })
+
 }
 
-const btnInfoData = async (id) =>{
+const btnInfoData = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
     const btnInfo = data.data
-    console.log(data)
-    
+    if (btnInfo.length === 0) {
+        console.log('i am 0');
+        const videoContain = document.getElementById('no-video-con');
+        videoContain.textContent = '';
+        videoContain.classList.remove('hidden')
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div>
+        <div class="flex justify-center">
+        <img src="Icon.png" alt="">
+        </div>
+        <h2 class="text-center text-4xl text-[#171717] font-bold mt-6">Oops!! Sorry, There is no<br>content here</h2>
+        </div>
+        `;
+        videoContain.appendChild(div)
+    } else {
+        const videoContain = document.getElementById('no-video-con');
+        videoContain.classList.add('hidden')
+
+    }
     const videoContain = document.getElementById('video-con');
     videoContain.textContent = '';
-    btnInfo.forEach((singleCard) =>{
-        console.log(singleCard)
+    btnInfo.forEach((singleCard) => {
+        // console.log(singleCard)
+
         const image = singleCard?.authors[0];
         const img = image?.profile_picture;
         const profileName = image?.profile_name;
         const div = document.createElement('div');
 
-        
+
         div.innerHTML = `
         <div class="rounded-lg h-[340px]">
                     <img class="h-56 w-full rounded-xl" src=${singleCard?.thumbnail}>
@@ -39,18 +61,21 @@ const btnInfoData = async (id) =>{
                         ${singleCard?.title}
                         </h3>
                     </div>
-                    <div class="flex gap-3 ml-12">
-                        <p class="text-sm text-[#171717B2] my-3">
-                        ${profileName}
-                        </p>
-                        <img class="" src="icon/icon.svg" alt="">
+                    <div class="flex justify-start ml-[52px] gap-4">
+                    <span> ${singleCard.authors[0].profile_name}</span>${singleCard.authors[0].verified ?
+                    '<img src="image.logo.svg" alt="" id="logo">' : ''}
                     </div>
-                    <p class="text-sm text-[#171717B2] ml-12">${singleCard?.others?.views
-                    }</p>
+                    <p class="text-sm text-[#171717B2] ml-[52px] mt-3">${singleCard?.others?.views
+            }</p>
                 </div>
         `;
         videoContain.appendChild(div);
     })
 }
+
+document.getElementById('qus-ans').addEventListener('click', function (){
+    window.location.href = 'qusans.html';
+})
+
 getBtnDataCollect();
 btnInfoData('1000')
