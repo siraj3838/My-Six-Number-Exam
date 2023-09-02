42a711a9500266a5f8cc5b8ab38b1d4bf4c3f722
Sirajul Islam <ayasabegum134@gmail.com>
2023-09-02 14:23:25 +0600

    my Happy Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const getBtnDataCollect = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
-    const btnData = data.data
+    const btnData = data.data;
     const allBtn = document.getElementById('receive-btn-data-api');
     btnData.forEach((single) => {
         const div = document.createElement('div');
@@ -10,21 +10,19 @@ const getBtnDataCollect = async () => {
         ${single.category}
         </button>
         `;
-
-
-        allBtn.appendChild(div)
-    })
-
+        allBtn.appendChild(div);
+    });
 }
 
 const btnInfoData = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
-    const btnInfo = data.data
+    const btnInfo = data.data;
+
     if (btnInfo.length === 0) {
         const videoContain = document.getElementById('no-video-con');
         videoContain.textContent = '';
-        videoContain.classList.remove('hidden')
+        videoContain.classList.remove('hidden');
         const div = document.createElement('div');
         div.innerHTML = `
         <div>
@@ -34,31 +32,26 @@ const btnInfoData = async (id) => {
         <h2 class="text-center text-4xl text-[#171717] font-bold mt-6">Oops!! Sorry, There is no<br>content here</h2>
         </div>
         `;
-        videoContain.appendChild(div)
+        videoContain.appendChild(div);
     } else {
         const videoContain = document.getElementById('no-video-con');
-        videoContain.classList.add('hidden')
+        videoContain.classList.add('hidden');
 
     }
+
     const videoContain = document.getElementById('video-con');
     videoContain.textContent = '';
     btnInfo.forEach((singleCard) => {
-
         const image = singleCard?.authors[0];
         const img = image?.profile_picture;
         const profileName = image?.profile_name;
         const div = document.createElement('div');
-
-        let secondNum = singleCard?.others?.posted_date
-        // console.log(secondNum)
+        let secondNum = singleCard?.others?.posted_date;
         let hour = Math.floor(secondNum / 60);
         let result = hour / 60;
         let result2 = parseInt(result);
         let resultFloat = parseFloat(result2);
         let minute = secondNum % 60;
-        console.log(resultFloat, minute)
-        
-
         div.innerHTML = `
         <div class="rounded-lg h-[340px] relative">
                     <img class="h-56 w-full rounded-xl" src=${singleCard?.thumbnail}>
@@ -79,12 +72,12 @@ const btnInfoData = async (id) => {
                 </div>
         `;
         videoContain.appendChild(div);
-    })
-}
+    });
+};
 
 document.getElementById('qus-ans').addEventListener('click', function (){
     window.location.href = 'qusans.html';
-})
+});
 
 getBtnDataCollect();
-btnInfoData('1000')
+btnInfoData('1000');
